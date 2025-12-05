5403d4d4b77e979e4482e792da839b68057dd3a3
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-05 10:50:45 +0000

    feat: Create blog app

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def test(request):
+    """Returns a response with a test message."""
+    return HttpResponse("blog app is working")
