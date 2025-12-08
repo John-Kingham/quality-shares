7ad54e5a2f26ee2413fc6f773d70861575a5de66
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-08 11:18:58 +0000

    feat: create blog.Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    A blog post.
+
+    Related:
+        :model:`auth.User`.
+    """
+
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    published = models.BooleanField(default=False)
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = CloudinaryField("image", default="placeholder")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
