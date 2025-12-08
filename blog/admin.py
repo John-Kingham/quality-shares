fb3baf8534520246ce9cac7cbd48d73dd22548a5
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-08 13:38:10 +0000

    feat: add summernote to blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ("title", "published", "created")
+    search_fields = ["title", "content"]
+    list_filter = ("published", "created")
+    prepopulated_fields = {"slug": ("title",)}
+    summernote_fields = ("content",)
