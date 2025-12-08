2e4b3d602e55f2b61f8bb16cb3685a99d520b306
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-08 10:52:07 +0000

    feat: install cloudinary

--- quality_shares/settings.py
+++ quality_shares/settings.py
@@ -48,7 +48,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
     "blog",
+    "cloudinary",
 ]
 
 MIDDLEWARE = [
