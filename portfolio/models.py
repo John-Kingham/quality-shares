d135707a4ba2620176720f44438b8d6a8a0bc0b0
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-09 14:20:53 +0000

    feat: add image field to portfolio model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class Portfolio(models.Model):
@@ -7,6 +8,8 @@ class Portfolio(models.Model):
     """
 
     title = models.CharField(max_length=200, unique=True)
+    image = CloudinaryField("image", default="placeholder")
+    image_caption = models.CharField(max_length=100, blank=True)
     content = models.TextField(blank=True)
     updated = models.DateTimeField(auto_now=True)
 
