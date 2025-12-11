aa23d1f8e3bc68469a6ebdf203b43959714bedea
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-11 09:43:49 +0000

    docs: add docstring for portfolio view

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,15 @@ from .models import Portfolio
 
 
 def portfolio_view(request):
+    """
+    Returns a response for the Portfolio page.
+
+    Args:
+        request (HttpRequest): A GET request.
+
+    Returns:
+        HttpResponse: Contains the Portfolio page.
+    """
     portfolio = Portfolio.objects.first()
     context = {"portfolio": portfolio}
     return render(request, "portfolio/portfolio.html", context)
