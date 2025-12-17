a64dafb7daa40aaa0ec1f9eea9c318aa1b6aa81c
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-17 20:36:58 +0000

    feat: install AllAuth and setup login/out buttons

--- quality_shares/settings.py
+++ quality_shares/settings.py
@@ -52,7 +52,15 @@ INSTALLED_APPS = [
     "cloudinary",
     "django_summernote",
     "portfolio",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -63,6 +71,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "quality_shares.urls"
@@ -133,6 +142,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = "none"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- quality_shares/urls.py
+++ quality_shares/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("portfolio/", include("portfolio.urls")),
     path("summernote/", include("django_summernote.urls")),
     path("", include("blog.urls")),
