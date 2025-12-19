1b71631f22ab54b3af2f7124de615949d8baab8f
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-12-19 14:39:17 +0000

    test: add About page tests

--- about/test_views.py
+++ about/test_views.py
@@ -1,25 +1,25 @@
 from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
-from .models import Portfolio
+from .models import About
 
 
-class TestPortfolioView(TestCase):
+class TestAboutView(TestCase):
 
     def setUp(self):
-        self.title = "Test Portfolio Title"
+        self.title = "Test About Title"
         self.image_caption = "Test image caption"
-        self.content = "Test portfolio content."
-        self.portfolio = Portfolio(
+        self.content = "Test About content."
+        self.about = About(
             title=self.title,
             image_caption=self.image_caption,
             content=self.content,
         )
-        self.portfolio.save()
-        self.response = self.client.get(reverse("portfolio"))
+        self.about.save()
+        self.response = self.client.get(reverse("about"))
 
-    def test_portfolio_page(self):
-        """That that the Portfolio page contains the correct information."""
+    def test_about_page(self):
+        """Test that the About page contains the correct information."""
         self.assertEqual(self.response.status_code, HTTPStatus.OK)
         self.assertContains(self.response, self.title)
         self.assertContains(self.response, self.image_caption)
